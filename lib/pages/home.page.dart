0252ab0db87666feeb4cae055da6425c1e0cf6b9
Nitish Kumar Singh <nitishk72@gmail.com>
2019-10-06 21:32:25 +0530

    removed  overflowed by x pixels on the bottom.

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -7,55 +7,57 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            SizedBox(
-              height: 60,
-            ),
-            SearchBox(),
-            SizedBox(
-              height: 30,
-            ),
-            Text(
-              "Categories",
-              style: Theme.of(context).textTheme.headline,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 90,
-              child: CategoryList(),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  "Best Selling",
-                  style: Theme.of(context).textTheme.headline,
-                ),
-                FlatButton(
-                  child: Text("See All"),
-                  onPressed: () => {},
-                ),
-              ],
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              height: 350,
-              child: ProductList(
-                scrollDirection: Axis.horizontal,
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              SizedBox(
+                height: 60,
               ),
-            ),
-          ],
+              SearchBox(),
+              SizedBox(
+                height: 30,
+              ),
+              Text(
+                "Categories",
+                style: Theme.of(context).textTheme.headline,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 90,
+                child: CategoryList(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Text(
+                    "Best Selling",
+                    style: Theme.of(context).textTheme.headline,
+                  ),
+                  FlatButton(
+                    child: Text("See All"),
+                    onPressed: () => {},
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                height: 350,
+                child: ProductList(
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
